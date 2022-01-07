a814e00088faa89fc97bad5401ffdc3cc539c6a9
ananishida <ananishida@outlook.com>
2022-01-06 21:28:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sigleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Singleton s1 = Singleton.getInstance();
+            Console.WriteLine(Singleton.nome);
+
+            Singleton s2 = Singleton.getInstance();
+            // Console.WriteLine(Singleton.nome);
+
+            if (s1==s2)
+            {
+                Console.WriteLine("As instancias são iguais!");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigleton
+{
+    public class Singleton
+    {
+        public static  Singleton instancia { get; set; }
+        public static string nome { get; set; }
+        public static Singleton getInstance()
+        {
+            if (instancia == null)
+            {
+                instancia = new Singleton();
+                nome = "Ana";
+
+            }
+            return instancia;
+        }
+    }
+}
